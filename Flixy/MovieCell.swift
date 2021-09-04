3a522a6375006e7605e524d52a68d05db64085f9
Ahmad Suleiman <60371882+MadSuleiman@users.noreply.github.com>
2021-09-04 01:03:41 -0700

    Initial commit

---
+++ Flixy/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flixy
+//
+//  Created by Ahmad Suleiman on 9/3/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
